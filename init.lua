2ca8ad018c6c306fb81b24a5ac7270bfc3439e61
gub <gub@live.ca>
2024-03-06 20:56:57 -0500

    added autopairs

--- init.lua
+++ init.lua
@@ -260,6 +260,18 @@ require('lazy').setup {
       require('neo-tree').setup {}
     end,
   },
+  {
+    'windwp/nvim-autopairs',
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
